05281fd4d5355d58e6ebaa18d425d0f2eb3405af
Jordan Shurmer <jshurmer@scrippsnetworks.com>
2018-10-30 14:27:21 -0400

    Discovery inherited ACLs as well

--- index.js
+++ index.js
@@ -8,19 +8,36 @@ const wac = module.exports = {}
  * Parse the Link header for the first rel="acl" link
  */
 wac.aclUrl = function (docUrl) {
-  console.debug(`Determining acl url for ${docUrl}`)
-  return fetch(docUrl, { method: 'HEAD' })
-    .then(response => {
-      let aclUrl = ''
-      const links = parseHeader(response.headers.get('Link'))
-      if (links && links.acl) {
-        // resolve the relative acl Link against the given document url
-        aclUrl = new URL(links.acl.url, docUrl).href
-        console.debug(`ACL URL from ${docUrl} Headers: ${aclUrl}`)
-      } else {
-        console.debug(`No ACL Link header for ${docUrl}`)
-      }
-      return aclUrl
-    })
+    console.debug(`Determining acl url for ${docUrl}`)
+    return fetch(docUrl, {method: 'HEAD'})
+        .then(response => {
+            let aclUrl = ''
+            const links = parseHeader(response.headers.get('Link'))
+            if (links && links.acl) {
+                // resolve the relative acl Link against the given document url
+                aclUrl = new URL(links.acl.url, docUrl).href
+                console.debug(`ACL URL from ${docUrl} Headers: ${aclUrl}`)
+
+                //check for existence of the doc
+                return fetch(aclUrl)
+                    .then(aclResponse => {
+                        if (aclResponse.ok) {
+                            return aclUrl;
+                        } else {
+                            //get the container url
+                            const container = new URL(docUrl.endsWith('/') ? '..' : '.', docUrl).href;
+                            if (container === docUrl) {
+                                //TODO: We've already checked all the way.. what now? error?
+                                return '';
+                            } else {
+                                return wac.aclUrl(container);
+                            }
+                        }
+                    });
+            } else {
+                console.debug(`No ACL Link header for ${docUrl}`)
+                return aclUrl
+            }
+        })
     // TODO: handle fetch.catch().  return ''? throw errors?
 }
--- index.test.js
+++ index.test.js
@@ -1,17 +1,23 @@
 const fetchMock = require('fetch-mock')
 const wac = require('./index.js')
 
+fetchMock.catch((url, config) => {
+    //provide the acl headers for all containers
+    if (url.endsWith("/"))
+        return {status: 200, headers: {Link: '<.acl>; rel="acl"'}};
+
+    //404 for any url not explicitly mocked
+    return 404;
+})
+
 test('Get ACL Url - no link header', async function () {
-        console.debug('Testing aclUrl(string) no link header')
-        const docUrl = 'http://example.com/no-acl'
-        fetchMock.head(docUrl, 200)
-        const aclUrl = await wac.aclUrl(docUrl)
-        expect(aclUrl).toBe('')
-    }
-)
+    const docUrl = 'http://example.com/no-acl'
+    fetchMock.head(docUrl, 200)
+    const aclUrl = await wac.aclUrl(docUrl)
+    expect(aclUrl).toBe('')
+});
 
 test('Get ACL Url - with link header', async function () {
-    console.debug('Testing aclUrl(string) direct link header')
     const docUrl = 'http://example.com/direct-acl'
     fetchMock.head(docUrl, {status: 200, headers: {Link: '<direct-acl.acl>; rel="acl"'}})
     fetchMock.get(`${docUrl}.acl`, 200)
@@ -19,3 +25,18 @@ test('Get ACL Url - with link header', async function () {
     expect(aclUrl).toBe('http://example.com/direct-acl.acl')
 });
 
+test('Get ACL Url - parent inherited ACL', async function () {
+    const docUrl = 'http://example.com/parent/inherited-acl'
+    fetchMock.head(docUrl, {status: 200, headers: {Link: '<inherited-acl.acl>; rel="acl"'}})
+    fetchMock.get('http://example.com/parent/.acl', 200)
+    const aclUrl = await wac.aclUrl(docUrl);
+    expect(aclUrl).toBe('http://example.com/parent/.acl')
+});
+
+test('Get ACL Url - deep inherited ACL', async function () {
+    const docUrl = 'http://example.com/more/parents/here/than/normal/urls/typically/have/in/general/deep-acl'
+    fetchMock.head(docUrl, {status: 200, headers: {Link: '<inherited-acl.acl>; rel="acl"'}})
+    fetchMock.get('http://example.com/.acl', 200)
+    const aclUrl = await wac.aclUrl(docUrl);
+    expect(aclUrl).toBe('http://example.com/.acl')
+});
